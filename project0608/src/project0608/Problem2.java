a04a58696fe3409de172c8cdfbc2214c7129a0dc
1 <bhanu prakash reddy@192.168.1.136>
2016-07-06 13:20:55 -0400

    hhnk uinjn jn

---
+++ project0608/src/project0608/Problem2.java
@@ -0,0 +1,16 @@
+package project0608;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		int i,j;
+		for (i=5; i>=1; i--){
+			for(j=5; j>=1; j--){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
